cbc51698ae17afde21492ae124bb1db8dad9464a
Augusto Peres <augustoapg@gmail.com>
2019-11-11 16:36:16 -0500

    Created initial layout for add costume page

--- app/src/main/java/sheridan/araujope/diyhalloweencostume/MainActivity.java
+++ app/src/main/java/sheridan/araujope/diyhalloweencostume/MainActivity.java
@@ -2,13 +2,29 @@ package sheridan.araujope.diyhalloweencostume;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.floating_action_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddCostume.class);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
     }
 }
