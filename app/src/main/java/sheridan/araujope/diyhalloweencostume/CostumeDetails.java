108b5b73b7c983cb340275aabd2e0200992674f0
Augusto Peres <augustoapg@gmail.com>
2019-11-18 17:18:23 -0500

    implemented recyclerView (image not working)

---
+++ app/src/main/java/sheridan/araujope/diyhalloweencostume/CostumeDetails.java
@@ -0,0 +1,14 @@
+package sheridan.araujope.diyhalloweencostume;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CostumeDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_costume_details);
+    }
+}
---
+++ app/src/main/java/sheridan/araujope/diyhalloweencostume/RecyclerViewAdapter.java
@@ -0,0 +1,4 @@
+package sheridan.araujope.diyhalloweencostume;
+
+public class RecyclerViewAdapter {
+}
